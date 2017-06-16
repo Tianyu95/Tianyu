c910af4b7c0c1260d5722866505512ed30c489f7
Tianyu <ctianyu95@gmail.com>
2017-06-16 10:02:14 +0800

    first page horse

--- Tianyu/FirstPage/TYFirstPageViewController.swift
+++ Tianyu/FirstPage/TYFirstPageViewController.swift
@@ -11,11 +11,26 @@ import UIKit
 class TYFirstPageViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollViewA: UIScrollView!
-    
     @IBOutlet weak var pageControlA: UIPageControl!
-    
-
     var timer: Timer?
+
+    
+    @IBOutlet weak var horseBgLabel: UILabel!
+    var contentLabel : UILabel?
+    var timeHorse : Timer?
+    var count = 0
+    var titleArray = ["美媒：建造世界最大望远镜计划掀起争论",
+                      "开车必备！新手上路的35个驾驶技巧！",
+                      "每天一万步，向着夏日好身材，一起动起来！",
+                      "10.5英寸iPad Pro实测支持29W快充.",
+                      "时间顺流而下，生活逆流而上...",
+                      "80%的时间都是和搭建页面。",
+                      "写业务逻辑、网络请求打交道。",
+                      "世界最大4K电视：262英寸屏幕 重1600斤！"]
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +68,24 @@ class TYFirstPageViewController: UIViewController, UIScrollViewDelegate {
         
         self.scrollViewA.contentSize = CGSize(width: widths * 6, height: heights)
         self.scrollViewA.contentOffset = CGPoint(x: widths, y: 0)
+        
+        
+        
+        //horse Race Lamp//////////////////////
+        contentLabel = UILabel(frame: CGRect(x: UIScreen.main.bounds.size.width, y: 0, width: self.horseBgLabel.frame.size.width, height: self.horseBgLabel.frame.size.height))
+        contentLabel?.text = titleArray[0]
+        contentLabel?.textAlignment = .left
+        contentLabel?.textColor = UIColor.black
+        contentLabel?.font = UIFont.systemFont(ofSize: 20)
+        horseBgLabel.addSubview(contentLabel!)
+        
+        self.timeHorse = Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector(self.nextLabel), userInfo: nil, repeats: true)
+        UIView.animate(withDuration: 4.0, delay: 4.0, options: [.curveEaseIn, .repeat], animations: {
+            var frame = self.contentLabel?.frame
+            frame?.origin.x = (frame?.origin.x)! - self.horseBgLabel.frame.size.width - (self.contentLabel?.frame.size.width)!
+            self.contentLabel?.frame = frame!
+        }, completion: nil)
+        
         
     }
 
@@ -126,18 +159,29 @@ class TYFirstPageViewController: UIViewController, UIScrollViewDelegate {
     ///停止滚动
     func pauseScroll()
     {
-//        let offset = self.scrollView!.contentOffset.x;
-//        let index = offset / self.imgWidth!
+//        let offset = self.scrollViewA!.contentOffset.x;
+//        let index = offset / self.widths!
 //        //1表示没有滚动
 //        if index == 1
 //        {
 //            return
 //        }
 //        self.currentIndex = self.nextIndex
-//        self.pageControl?.currentPage = self.currentIndex!
+//        self.pageControlA?.currentPage = self.currentIndex!
 //        self.currentView?.frame = CGRect(x: self.imgWidth!, y: 0, width: self.imgWidth!, height: self.imgHeight!)
 //        self.currentView?.backgroundColor = self.nextView?.backgroundColor
 //        self.scrollView?.contentOffset = CGPoint(x: imgWidth!, y: 0)
     }
     
+    
+    
+    func nextLabel()  {
+        let index = self.count % self.titleArray.count
+        self.contentLabel?.text = self.titleArray[index]
+        self.count += 1
+        if self.count == self.titleArray.count {
+            self.count = 0
+        }
+        
+    }
 }
